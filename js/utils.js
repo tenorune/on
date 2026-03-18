e4bfa3e478bbb9f4f86f3176e7542d9e28ef7de7
tenorune <tenorune@users.noreply.github.com>
2026-03-18 02:10:32 +0100

    feat: add hexToRgb helper to utils.js

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,9 @@ export function escapeHtml(str) {
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#39;');
 }
+
+export function hexToRgb(hex) {
+  const m = /^#?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i.exec(hex);
+  if (!m) return '0, 0, 0';
+  return `${parseInt(m[1], 16)}, ${parseInt(m[2], 16)}, ${parseInt(m[3], 16)}`;
+}
