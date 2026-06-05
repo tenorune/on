f7fb0b174837b7328f5f879214e142be68bff8b8
tenorune <117549102+tenorune@users.noreply.github.com>
2026-06-05 06:09:59 +0000

    test: exclude functions/ from root jest (separate node-env toolchain)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: { '^.+\\.js$': 'babel-jest' },
   passWithNoTests: true,
+  // functions/ has its own node-env Jest config; keep the two toolchains separate.
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/functions/'],
   setupFilesAfterEnv: ['./tests/setup-globals.js'],
 };
